ad539dfade769b9b881e1571d78aa32829a49b70
shuqi-mo <moshq7@foxmail.com>
2021-04-15 08:48:11 +0800

    improve output format and error position

--- lab3/实验软装置（后缀表达式Postfix）/src/Postfix.java
+++ lab3/实验软装置（后缀表达式Postfix）/src/Postfix.java
@@ -6,6 +6,7 @@ class Parser {
 	private int digitcnt;
 	private int len;
 	private boolean error;
+	private StringBuffer result = new StringBuffer();
 
 	public Parser() throws IOException {
 		this.operatorcnt = 0;
@@ -22,6 +23,7 @@ class Parser {
 	void expr() throws IOException {
 		term();
 		rest();
+		System.out.println("后缀表达式输出结果："+result);
 	}
 
 	void rest() throws IOException {
@@ -30,13 +32,13 @@ class Parser {
 				match('+');
 				term();
 				if(!error)
-					System.out.write('+');
+					result.append('+');
 				continue;
 			} else if (lookahead == '-') {
 				match('-');
 				term();
 				if(!error)
-					System.out.write('-');
+					result.append('-');
 				continue;
 			} else {
 				break;
@@ -47,17 +49,17 @@ class Parser {
 	void term() throws IOException {
 		error = false;
 		if(operatorcnt > digitcnt) {
-			System.out.println("\nlackLeftItem");
+			System.out.println("语法错误：输入字符串中的第"+len+"个字符缺少左运算量，输出结果将跳过该字符");
 			operatorcnt--;
 			match(lookahead);
 		}
 		else if(operatorcnt == digitcnt) {
-			System.out.println("\nlackRightItem");
+			System.out.println("语法错误：输入字符串中的第"+(len-1)+"个字符缺少右运算量，输出结果将跳过该字符");
 			error = true;
 			match(lookahead);
 		}
 		if(Character.isDigit((char)lookahead)) {
-			System.out.write((char)lookahead);
+			result.append((char)lookahead);
 			match(lookahead);
 		}
 	}
@@ -71,7 +73,7 @@ class Parser {
 			if(Character.isDigit((char)lookahead)) digitcnt++;
 			else if(lookahead == '+' || lookahead == '-') operatorcnt++;
 			if(digitcnt - 2 >= operatorcnt) {
-				System.out.println("\nlackOperator");
+				System.out.println("语法错误：输入字符串中的第"+(len-1)+"个字符与第"+len+"个字符之间缺少运算符，输出结果将跳过后一字符");
 				digitcnt--;
 				match(lookahead);
 			}
@@ -86,7 +88,7 @@ class Parser {
 		else if(lookahead == '+' || lookahead == '-' || lookahead == 13 || lookahead == 10)
 			return true;
 		else {
-			System.out.println("illegal input!");
+			System.out.println("词法错误：输入字符串中的第"+len+"个字符不是合法字符，输出结果将跳过该字符");
 			return false;
 		}
 	}
@@ -96,6 +98,6 @@ public class Postfix {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Input an infix expression and output its postfix notation:");
 		new Parser().expr();
-		System.out.println("\nEnd of program.");
+		System.out.println("End of program.");
 	}
 }
